5cbca965101cce9de10aa18a042f78eee27b9203
Vaal34 <5619@holbertonstudents.com>
2024-01-12 23:16:23 +0100

    test

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,24 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-listint_t *insert_node(listint_t **head, int number) {
-    
+listint_t *insert_node(listint_t **head, int number)
+{
     listint_t *new_node;
 
-    new_node = (listint_t *) malloc(sizeof(listint_t));
+    if (head == NULL)
+        return (NULL);
 
-    if (new_node == NULL) {
-        return NULL;
-    }
+    new_node = malloc(sizeof(listint_t));
+    if (!new_node)
+        return (NULL);
 
     new_node->n = number;
-    new_node->next = *head;
+    new_node->next = NULL;
 
-    while(new_node->next != NULL) {
-        if(new_node->next->n < new_node->n) {
-            new_node = new_node->next;
-        };
-    };
+    while (*head != NULL && number > (*head)->n)
+    {
+        head = &(*head)->next;
+    }
+
+    new_node->next = *head;
+    *head = new_node;
 
     return (new_node);
 }
