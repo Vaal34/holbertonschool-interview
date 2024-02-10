f81c8ec77d293aa90ff3f05c89078d998f34e727
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-10 19:59:13 +0100

    if list have one number is a palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -18,7 +18,7 @@ int is_palindrome(listint_t **head) {
     temp = *head;
 
     if (*head == NULL) {
-        return 1    ;
+        return 1;
     }
 
     while ((*head)->next != NULL) {
@@ -31,7 +31,7 @@ int is_palindrome(listint_t **head) {
     if (length_list % 2 != 0) {
         return 0;
     } else if (length_list == 1) {
-        return 1
+        return 1;
     }
 
     First_part = (int *)malloc((length_list / 2) * sizeof(int));
