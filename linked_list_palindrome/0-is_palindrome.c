8dfa9423d66196103d68673cec8569c838be2571
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-10 19:55:30 +0100

    fix compilation error

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,71 +1,65 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-*
-*
-*
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * An empty list is considered a palindrome
 **/
 
 int is_palindrome(listint_t **head) {
+    int length_list = 1;
+    listint_t *temp;
+    int *First_part, *Second_part;
+    int i, j, fi, si;
 
-
-    listint_t *tmp;
-
-    tmp = *head;
+    temp = *head;
 
     if (*head == NULL) {
-        return 1;
+        return (1);
     }
 
-    int length_list = 1;
-
-    while((*head)->next != NULL) {
+    while ((*head)->next != NULL) {
         *head = (*head)->next;
         length_list++;
     }
 
-    *head = tmp;
+    *head = temp;
 
-    int *First_part = (int*)malloc((length_list / 2) * sizeof(int));
-    int *Second_part = (int*)malloc((length_list / 2) * sizeof(int));
+    if (length_list % 2 != 0) {
+        return (0);
+    }
 
-    for(int i = 0; i < (length_list / 2); i++) {
+    First_part = (int *)malloc((length_list / 2) * sizeof(int));
+    Second_part = (int *)malloc((length_list / 2) * sizeof(int));
+
+    for (i = 0; i < (length_list / 2); i++) {
         First_part[i] = (*head)->n;
         *head = (*head)->next;
     }
 
-    for(int j = 0; j < (length_list / 2); j++) {
+    for (j = 0; j < (length_list / 2); j++) {
         Second_part[j] = (*head)->n;
         *head = (*head)->next;
     }
 
-    /*
-    for(int fi = 0; fi < (length_list / 2); fi++) {
-        printf("+++++++++++%i++++++++++", First_part[fi]);
-        for(int si = (length_list / 2 - 1); si > 0; si--) {
-            printf("---s--%i--s-------", Second_part[si]);
-            if(First_part[fi] == Second_part[si]) {
-                break;
-            } else {
-                printf("caca");
-                return 0;
-            }
-        }
-    }
-    */
-    
-    int fi = 0;
-    int si = (length_list / 2 - 1);
+    fi = 0;
+    si = (length_list / 2 - 1);
 
-    while(fi < (length_list / 2)) {
-        if(First_part[fi] == Second_part[si]) {
+    while (fi < (length_list / 2)) {
+        if (First_part[fi] == Second_part[si]) {
         } else {
-            printf("caca");
+            free(First_part);
+            free(Second_part);
             return 0;
         }
         fi++;
         si--;
     }
+
+    free(First_part);
+    free(Second_part);
     return 1;
 }
