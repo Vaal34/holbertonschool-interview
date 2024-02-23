6d627d002afeb293bb932a6d971d1a6fb0874c5a
Vaal34 <5619@holbertonstudents.com>
2024-02-23 13:41:00 +0100

    check cycle linked lsit

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -9,39 +9,18 @@
 #include "lists.h"
 
 int check_cycle(listint_t *list) {
-    listint_t *current_node = list;
-    listint_t *tmp = list;
-    int length_list = 0;
+    if (list == NULL || list->next == NULL)
+        return 0;
+    
+    listint_t *before = list;
+    listint_t *after = list->next;
 
-    if (list == NULL || list->next == NULL){
-        return 1;
+    while (after != NULL && after->next != NULL) {
+        if (before == after)
+            return 1;
+        before = before->next;
+        after = after->next->next;
     }
-
-    while (current_node != NULL) {
-        current_node = current_node->next;
-        length_list++;
-    }
-
-    current_node = tmp;
-
-    int *Linked_List = (int *)malloc(length_list * sizeof(int));
-
-    int i = 0;
-    while (current_node != NULL) {
-        Linked_List[i] = current_node->n;
-        current_node = current_node->next;
-        i++;
-    }
-
-    for (int i = 0; i < length_list; i++) {
-        for (int j = i + 1; j < length_list; j++) {
-            if (Linked_List[i] == Linked_List[j]) {
-                free(Linked_List);
-                return 1;
-            }
-        }
-    }
-
-    free(Linked_List);
+    
     return 0;
 }
