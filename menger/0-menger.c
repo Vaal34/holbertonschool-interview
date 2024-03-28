0be6f2c49c34a0cef1b1ebfe699fc434cf524e54
Vaal34 <5619@holbertonstudents.com>
2024-03-28 14:53:33 +0100

    fix error compil

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -2,13 +2,14 @@
 #include <math.h>
 
 void drawMenger(int level, int x, int y, int size) {
+    int i, j; // Declaring variables at the beginning of the block
     if (level == 0) {
         printf("#");
         return;
     }
     int newSize = size / 3;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
             if (i == 1 && j == 1)
                 printf(" ");
             else
@@ -20,9 +21,10 @@ void drawMenger(int level, int x, int y, int size) {
 void menger(int level) {
     if (level < 0)
         return;
-    int size = pow(3, level);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    int i, j; // Declaring variables at the beginning of the block
+    int size = (int)pow(3, level);
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
             drawMenger(level, i, j, size);
         }
         printf("\n");
