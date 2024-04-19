9a686a5d8821a8e5907cef83f4ad15d8e0d86600
Vaal34 <5619@holbertonstudents.com>
2024-04-19 12:10:00 +0200

    betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -19,40 +19,32 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	skiplist_t *newNode = list, *express = list->express, *last = newNode;
 
-	while (express != NULL && express->n < value)
-	{
+	while (express != NULL && express->n < value) {
 		printf(CHECKED_VALUE, express->index, express->n);
 		newNode = express;
 		express = express->express;
 	}
 
-	if (express != NULL)
-	{
+	if (express != NULL) {
 		printf(CHECKED_VALUE, express->index, express->n);
-	}
-	else {
+	} else {
 		last = newNode;
 		while (last->next != NULL)
 			last = last->next;
 	}
 
-	if (express != NULL)
-	{
+	if (express != NULL) {
 		printf(FOUND_BETWEEN_INDEXES, newNode->index, express->index);
-	}
-	else
-	{
+	} else {
 		printf(FOUND_BETWEEN_INDEXES, newNode->index, last->index);
 	}
 
-	while (newNode != NULL && newNode->n < value)
-	{
+	while (newNode != NULL && newNode->n < value) {
 		printf(CHECKED_VALUE, newNode->index, newNode->n);
 		newNode = newNode->next;
 	}
 
-	if (newNode != NULL && newNode->n == value)
-	{
+	if (newNode != NULL && newNode->n == value) {
 		printf(CHECKED_VALUE, newNode->index, newNode->n);
 		return newNode;
 	}
