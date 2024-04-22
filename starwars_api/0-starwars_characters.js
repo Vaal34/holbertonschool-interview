2b733b85e4ab075beeaf20f1ff704df8da00e415
Vaal34 <5619@holbertonstudents.com>
2024-04-22 07:45:03 +0200

    request to character on film

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+const request = require("request");
+
+const numFilm = process.argv[2];
+
+request(`https://swapi-api.hbtn.io/api/films/${numFilm}/`, function(err, res, body){
+    if(res.statusCode != 200){
+        console.log(err);
+    }
+    movieJson = JSON.parse(body)
+    const allCharacters = movieJson.characters
+    allCharacters.forEach(character => {
+        request(character, function(err, res, body){
+            const characterJSON = JSON.parse(body)
+            console.log(characterJSON.name);
+        })
+    });
+})
