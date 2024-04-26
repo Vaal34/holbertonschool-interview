98d0dfac75812d27dd06de60dc328a774d89e5b0
Vaal34 <5619@holbertonstudents.com>
2024-04-26 14:53:54 +0200

    Star Wars Characters

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,19 +1,47 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
+const movieId = process.argv[2];
 
-const numFilm = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(`https://swapi-api.hbtn.io/api/films/${numFilm}/`, function(err, res, body){
-    if(res.statusCode != 200){
-        console.log(err);
-    }
-    movieJson = JSON.parse(body)
-    const allCharacters = movieJson.characters
-    allCharacters.forEach(character => {
-        request(character, function(err, res, body){
-            const characterJSON = JSON.parse(body)
-            console.log(characterJSON.name);
-        })
+function getCharacterName(url) {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Failed to get character data: ${response.statusCode}`));
+      } else {
+        resolve(body.name);
+      }
     });
-})
+  });
+}
+
+async function fetchApi(url) {
+  try {
+    const filmData = await new Promise((resolve, reject) => {
+      request(url, { json: true }, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(new Error(`Failed to get film data: ${response.statusCode}`));
+        } else {
+          resolve(body);
+        }
+      });
+    });
+
+    const characterNames = [];
+    for (const characterUrl of filmData.characters) {
+      const name = await getCharacterName(characterUrl);
+      characterNames.push(name);
+    }
+    characterNames.forEach(name => console.log(name));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchApi(url);
