3b3453920241c8d2f8f5cb17f18215d286004f03
Vaal34 <5619@holbertonstudents.com>
2024-07-04 16:57:42 +0200

    test compile on iso90

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void merge(int *array, int *left, int left_size, int *right, int right_size);
 void merge_sort(int *array, size_t size);
 void print_array(int *array, size_t size);
 
